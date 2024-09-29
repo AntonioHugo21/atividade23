4faba93e9dd34d36412f9d42a7822e4c6107cfc0
AntonioHugo21 <antonio.pierre@aluno.ce.gov.br>
2024-09-29 12:10:14 -0300

    tabuada de um número

---
+++ atividade23.py
@@ -0,0 +1,15 @@
+# Crie um programa que receba um número do usuário e exiba a tabuada desse número de 1 a 10.
+
+número = int(input("Digite um numero: "))
+print(número * 1)
+print(número * 2)
+print(número * 3)
+print(número * 4)
+print(número * 5)
+print(número * 6)
+print(número * 6)
+print(número * 8)
+print(número * 9)
+print(número * 10)
+
+print('FIM!')
